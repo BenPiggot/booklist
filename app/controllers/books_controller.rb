2fac7103f4f173c0101110791a942941746d3b73
ben <wbpiggot@gmail.com>
2017-01-06 17:43:49 -0800

    Basic crud app in place

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,45 @@
+class BooksController < ApplicationController
+
+  def index
+    @books = Book.all
+  end
+
+  def new
+    @book = Book.new
+    render layout: false
+  end
+
+  def edit
+    @book = Book.find(params[:book])
+    render layout: false
+  end
+
+  def update 
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to books_path(@book)
+  end
+
+  def create
+    @book = Book.create(book_params)
+    redirect_to books_path
+  end
+
+  def show 
+    @book = Book.find(params[:id])
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:name, :author, :description, :topic, :isbn, :image)
+  end
+
+
+end
